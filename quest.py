2500e11f1583525bcf291f582f54a4714681c4aa
Ankit Anand <ankit0.anand0@gmail.com>
2025-09-26 19:08:38 +0530

    Remove unwanted notebooks

---
+++ quest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+#---------------------------------
+# Author: Ankit Anand
+# Date: 25/09/25
+# Email: ankit0.anand0@gmail.com
+#---------------------------------
+
+
+#-----------------------------------------------
+# python quest.py -i notebooks/ -o htmls/ --log
+# python quest.py -i notebooks/ -o htmls/
+#-----------------------------------------------
+
+class Quest:
+	def __init__(self):
+		self._notebooks_dir = notebooks_dir
+		self._output_dir = output_dir # Location to save the html files
+		self._want_to_log = True # Do not create a tutorials.csv with notebooks details
+	
+	
+	def ipynb_to_html(self):
+		pass
+	
+	
+	
+	
+	
